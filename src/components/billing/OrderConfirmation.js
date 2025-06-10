4b344c5e92728a90b0059bb807d53a59e00f3f7e
leo0611dias <127952394+leo0611dias@users.noreply.github.com>
2025-06-10 17:16:37 -0300

    Update OrderConfirmation.js

--- src/components/billing/OrderConfirmation.js
+++ src/components/billing/OrderConfirmation.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react';
 import SuccessIcon from "../../img/success.png";
+import OrderContext from '../../context/orderContext';
 
 const OrderConfirmation = () => {
+  const { checkoutDetails } = useContext(OrderContext);
 
-    return (
-        <>
-            <center>
-                <img alt="" className="img-circle" src={SuccessIcon} width="50px" height="50px" />
-                <br /><br />
-                <h4>Your order has been successfully placed!</h4>
-                <br /><br />
-            </center>
-        </>
-    )
-}
-export default OrderConfirmation
+  useEffect(() => {
+    fetch("http://localhost:3001/usuarios", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(checkoutDetails)
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error("Erro ao enviar pedido");
+        console.log("Pedido enviado com sucesso!");
+      })
+      .catch((err) => {
+        console.error("Erro:", err);
+      });
+  }, [checkoutDetails]);
+
+  return (
+    <center>
+      <img alt="" className="img-circle" src={SuccessIcon} width="50px" height="50px" />
+      <br /><br />
+      <h4>Your order has been successfully placed!</h4>
+      <br /><br />
+    </center>
+  );
+};
+
+export default OrderConfirmation;
