aff12a974dd41b50902b8a211a9e0c74f0d42656
leo0611dias <127952394+leo0611dias@users.noreply.github.com>
2025-06-10 16:58:49 -0300

    Add files via upload

---
+++ src/components/common/MultiStepForm/NavigateButton.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NavigationButton = ({ goNext, goPrevious, selectedIndex, list, proceedNext }) => (
+    <>
+        <div className="navigateBtn">
+            <button disabled={selectedIndex === 0} onClick={goPrevious} className="btn btn-primary">
+                Previous
+            </button>{" "}
+            {selectedIndex !== list.length - 1 && (
+                <button onClick={goNext} disabled={!proceedNext} className="btn btn-primary">
+                    {selectedIndex !== list.length - 1 ? "Next" : "Finish"}
+                </button>
+            )}
+        </div>
+    </>
+);
+
+export default NavigationButton;
