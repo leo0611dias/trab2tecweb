e31d55688d8943bdccf294f41e8d002dc7b4d9d0
leo0611dias <127952394+leo0611dias@users.noreply.github.com>
2025-06-11 16:21:24 -0300

    Update MultiStepForm.js

--- src/components/common/MultiStepForm/MultiStepForm.js
+++ src/components/common/MultiStepForm/MultiStepForm.js
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import NavigationButton from "./NavigateButton";
 import ProgressBar from "./ProgressBar";
 
+/**
+ * Espera-se que cada componente de etapa (list[n].component) aceite uma prop "ref"
+ * para expor o método validate.
+ */
 function MultiStepForm({ list, displayProgressBar, proceedNext = true }) {
-
     const [selectedIndex, setSelectedIndex] = useState(0);
+    const stepRefs = useRef([]);
 
-    const goNext = () => {
+    // Avançar apenas se validado
+    const goNext = async () => {
+        const ref = stepRefs.current[selectedIndex];
+        if (ref && typeof ref.validate === "function") {
+            const valid = await ref.validate();
+            if (!valid) return; // Não avança se inválido
+        }
         if (selectedIndex !== list.length - 1)
             setSelectedIndex(selectedIndex + 1);
     };
@@ -21,21 +31,21 @@ function MultiStepForm({ list, displayProgressBar, proceedNext = true }) {
             <div className={"wrapper wrapper-content "}>
                 <br />
                 <div className="row">
-                    <div className="col-lg-3">
+                    <div className="col-lg-3"></div>
+                </div>
+                {displayProgressBar && (<ProgressBar list={list} selectedIndex={selectedIndex} />)}
+                <div className="row form-container">
+                    <div className="col-lg-12">
+                        {
+                            React.cloneElement(
+                                list[selectedIndex].component,
+                                { ref: el => stepRefs.current[selectedIndex] = el }
+                            )
+                        }
                     </div>
                 </div>
-
-                {displayProgressBar && (<ProgressBar list={list} selectedIndex={selectedIndex} />)}
-
-                {/*-------------------------------*/}
-                <div className="row form-container">
-                    <div className="col-lg-12">{list[selectedIndex].component}</div>
-                </div>
-                {/*-------------------------------*/}
                 <div className="row">
                     <div className="col-lg-12">
-
-                        {/** ---------------- Navigation buttons ---------------- */}
                         <div className="modal-footer">
                             <div className="col-lg-6">
                                 <NavigationButton
@@ -48,7 +58,6 @@ function MultiStepForm({ list, displayProgressBar, proceedNext = true }) {
                             </div>
                         </div>
                     </div>
-                    {/** ---------------------------------------------------- */}
                 </div>
             </div>
         </>
